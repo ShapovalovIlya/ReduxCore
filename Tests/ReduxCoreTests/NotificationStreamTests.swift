c0d8d792b9effedcc54c70df12153bbf680895af
Ilia Shapovalov <greateman@mail.ru>
2024-05-15 02:06:05 +0300

    Add NotificationCenter stream

---
+++ Tests/ReduxCoreTests/NotificationStreamTests.swift
@@ -0,0 +1,45 @@
+//
+//  NotificationStreamTests.swift
+//  
+//
+//  Created by Илья Шаповалов on 15.05.2024.
+//
+
+import XCTest
+import ReduxStream
+
+final class NotificationStreamTests: XCTestCase {
+    private var notifications: NotificationCenter!
+    
+    override func setUp() async throws {
+        try await super.setUp()
+        
+        notifications = .default
+    }
+    
+    override func tearDown() async throws {
+        try await super.tearDown()
+        
+        notifications = nil
+    }
+    
+    func test_emitNotification() async {
+        let notify = Notification.Name("baz")
+        let sut = notifications.stream(of: notify)
+        let arr = NSMutableArray()
+        
+        let task = Task {
+            for await name in sut.map(\.name) {
+                arr.add(name)
+            }
+            
+        }
+        
+        notifications.post(name: notify, object: nil)
+        notifications.post(name: notify, object: nil)
+        notifications.post(name: notify, object: nil)
+        
+        await task.value
+        XCTAssertEqual(arr, [notify, notify, notify])
+    }
+}
